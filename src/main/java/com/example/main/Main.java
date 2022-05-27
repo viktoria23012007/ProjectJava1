f35bc53e69d1830df07350353037ee827afb3205
Виктория Вареник <viktoria.varenik@yandex.ru>
2022-05-27 20:07:39 +0300

    Добавленна программа расчета порций

---
+++ src/main/java/com/example/main/Main.java
@@ -0,0 +1,17 @@
+package com.example.main;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Привет, Нетология!");
+        System.out.println("2 + 3");
+        System.out.println(2 + 3);
+        int speed = 17;
+        int x = speed + 3;
+
+        double d = 6.5;
+
+        String name = "Petya";
+
+        System.out.println(name + " ехал со скоростью " + speed);
+    }
+}
